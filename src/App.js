4a32d21d5a9bdf5801949ffb03f85d4df5001ee6
akhilesh90634-star <akhilesh90634@gmail.com>
2026-03-10 11:17:42 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import Reg from './components/customer/Reg';
+
+// function App() {
+//   return (
+//     <div>
+//       <Reg/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Reg from './components/customer/Reg'
+import Login from './components/customer/Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Reg />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
